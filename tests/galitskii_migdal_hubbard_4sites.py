2c1d1b84ce79b20ce3e475480ea839ea294b9a5d
Simon Verret <verret.simon@gmail.com>
2026-04-13 16:14:13 -0400

    Fixed the n=3 and n=5 cases with exact GS + added the n=6 case

--- tests/galitskii_migdal_hubbard_4sites.py
+++ tests/galitskii_migdal_hubbard_4sites.py
@@ -32,23 +32,21 @@ def hubbard(hop=np.array([[0, 1], [1, 0]]), u=4, mu=None):
     return MAPPER.map(-hopping + u * interaction - mu * occupation)
 
 
-n = 3
+n = 6
 u = 4
-mu = u / 2 + 0.01
+mu = u/2
 max_iter = None
 
 hopping = np.diag(np.ones(n - 1), 1) + np.diag(np.ones(n - 1), -1)
-hopping = np.array([[0, 1, 0], [1, 0, 1.1], [0, 1.1, 0]])
 hamiltonian = hubbard(hopping, u, mu=mu)
 h_mu = (mu) * np.diag(np.ones(n))
 h_0 = -hopping - h_mu
 eigvals, eigvecs = np.linalg.eigh(hamiltonian.to_matrix())
 true_gs_energy = eigvals[0]
 true_gs_vector = eigvecs[:, 0]
-print(eigvals)
 
+degen_gs_vectors = eigvecs[:,(eigvals-eigvals.min())<1e-9].T
 
-# %%
 
 from LiouvilleLanczos.Lanczos import Lanczos
 from LiouvilleLanczos.Green import CF_Green, PolyLehmann_Green, Green_matrix
@@ -59,25 +57,12 @@ from LiouvilleLanczos.matrix_impl import (
 )
 
 
-def make_green_stack(
-    opset_list=["0-123", "1-2"],
-    max_iter=10,
-    n=4,
-    u=4,
-    mu=None,
-    energy=None,
-    backend="exact",
-    shots=np.inf,
-    eps=1e-17,
-    overwrite=False,
-):
+def make_green_stack(ground_state, opset_list=["0-123", "1-2"], max_iter=10, n=4, down=False):
     stacked_green = None
     abm_dict = {}
     min_iter = 1
     for opset in opset_list:
-        a, b, m = get_abmu(
-            max_iter, n, u, mu, energy, backend, shots, eps, opset, overwrite
-        )
+        a, b, m = get_abmu(ground_state, max_iter, n, opset, down=down)
         abm_dict[opset] = (a, b, m)
         min_iter = max(min_iter, len(a))
 
@@ -98,42 +83,36 @@ def annihilation_operators(n):
     ]
     return [MAPPER.map(c) for c in c_fermi_list]
 
+def annihilation_operators_down(n):
+    c_fermi_list = [
+        FermionicOp({f"-_{i+n}": 1}, num_spin_orbitals=2 * n) for i in range(n)
+    ]
+    return [MAPPER.map(c) for c in c_fermi_list]
 
-def get_abmu(
-    max_iter=10,
-    n=4,
-    u=4,
-    mu=None,
-    energy=None,
-    backend="exact",
-    shots=np.inf,
-    eps=1e-17,
-    opset="0-123",
-    overwrite=False,
-):
 
-    c_operators = annihilation_operators(n)
-
+def get_abmu(ground_state, max_iter=10, n=4, opset="0-123", down=False):
+    if down:
+        c_operators = annihilation_operators_down(n)
+    else:
+        c_operators = annihilation_operators(n)
+    
     main_i, other_i = opset.split("-")
     main_op = c_operators[int(main_i)]
     other_ops = [c_operators[int(i)] for i in other_i]
 
-    if backend == "exact":
-        _, eigvecs = np.linalg.eigh(hamiltonian.to_matrix())
-        ground_state = eigvecs[:, 0]
-        lanczos = Lanczos(
-            MatrixState_inner_product(ground_state),
-            Matrix_Liouvillian(),
-            Matrix_sum(),
-            logger=None,
-        )
-        a, b, mm = lanczos.polynomial_hybrid(
-            hamiltonian.to_matrix(),
-            main_op.to_matrix(),
-            [o.to_matrix() for o in other_ops],
-            max_iter,
-        )
-        return a, b, mm
+    lanczos = Lanczos(
+        MatrixState_inner_product(ground_state),
+        Matrix_Liouvillian(),
+        Matrix_sum(),
+        logger=None,
+    )
+    a, b, mm = lanczos.polynomial_hybrid(
+        hamiltonian.to_matrix(),
+        main_op.to_matrix(),
+        [o.to_matrix() for o in other_ops],
+        max_iter,
+    )
+    return a, b, mm
 
 
 def make_green_list(a, b, mm, min_iter=1):
@@ -154,11 +133,6 @@ def make_green_list(a, b, mm, min_iter=1):
             )
     return green_list
 
-
-true_stack_green = make_green_stack(
-    ["0-12", "1-02", "2-01"], 31, n, u, backend="exact", eps=1e-17
-)
-# %%
 from scipy.sparse import csr_array
 from typing import Callable
 from LiouvilleLanczos.Green import integrable_Green_function_base
@@ -168,13 +142,11 @@ def fermi(w):
     return w <= 0.0
 
 
-green = true_stack_green
-h0 = h_0
-
 
 # Hardcode Green mapping lines for 2-3-4-5 sites
 def green_mapping_line(n):
     if n == 2:
+        opset = ["0-1"]
         green_mapping = [  # 0-1
             (0, 0, 0, 1),
             (1, 1, 0, 1),
@@ -182,6 +154,7 @@ def green_mapping_line(n):
             (1, 0, 1, 1),
         ]
     if n == 3:
+        opset = ["0-12", "1-"]
         green_mapping = [  # 0-12 1    (4 Green)
             (0, 0, 0, 1),
             (2, 2, 0, 1),
@@ -194,7 +167,8 @@ def green_mapping_line(n):
             (1, 1, 3, 1),
         ]
     if n == 4:
-        green_mapping = [  # 0-123 1-2 (6 Green)
+        opset = ["0-123", "1-2"]
+        green_mapping = [
             (0, 0, 0, 1),
             (3, 3, 0, 1),
             (0, 1, 1, 1),
@@ -213,7 +187,8 @@ def green_mapping_line(n):
             (2, 1, 5, 1),
         ]
     if n == 5:
-        green_mapping = [  # 0-1234 1-23 2 (9 Green)
+        opset = ["0-1234", "1-23", "2-"]
+        green_mapping = [
             (0, 0, 0, 1),
             (4, 4, 0, 1),
             (0, 1, 1, 1),
@@ -240,89 +215,137 @@ def green_mapping_line(n):
             (3, 1, 7, 1),
             (2, 2, 8, 1),
         ]
-    return green_mapping
+    if n == 6:
+        opset = ["0-12345", "1-234", "2-3"]  ## 12 greens
+        green_mapping = [
+            #0-
+            (0, 0, 0, 1),
+            (5, 5, 0, 1),
+            #0-1
+            (0, 1, 1, 1),
+            (1, 0, 1, 1),
+            (4, 5, 1, 1),
+            (5, 4, 1, 1),
+            #0-2
+            (0, 2, 2, 1),
+            (2, 0, 2, 1),
+            (3, 5, 2, 1),
+            (5, 3, 2, 1),
+            #0-3
+            (0, 3, 3, 1),
+            (3, 0, 3, 1),
+            (5, 2, 3, 1),
+            (2, 5, 3, 1),
+            #0-4
+            (0, 4, 4, 1),
+            (4, 0, 4, 1),
+            (5, 1, 4, 1),
+            (1, 5, 4, 1),
+            #0-5
+            (0, 5, 5, 1),
+            (5, 0, 5, 1),
+            #1-
+            (1, 1, 6, 1),
+            (4, 4, 6, 1),
+            #1-2
+            (1, 2, 7, 1),
+            (2, 1, 7, 1),
+            (4, 3, 7, 1),
+            (3, 4, 7, 1),
+            #1-3
+            (1, 3, 8, 1),
+            (3, 1, 8, 1),
+            (4, 2, 8, 1),
+            (2, 4, 8, 1),
+            #1-4
+            (1, 4, 9, 1),
+            (4, 1, 9, 1),
+            #2-
+            (2, 2, 10, 1),
+            (3, 3, 10, 1),
+            #2-3
+            (2, 3, 11, 1),
+            (3, 2, 11, 1),
+        ]
+    return opset, green_mapping
 
 
-green_mapping_full = [
-    (0, 0, 0, 1),
-    (0, 1, 1, 1),
-    (0, 2, 2, 1),
-    (1, 0, 4, 1),
-    (1, 1, 3, 1),
-    (1, 2, 5, 1),
-    (2, 0, 7, 1),
-    (2, 1, 8, 1),
-    (2, 2, 6, 1),
-]
+spin_gm_energies = []
+for down in [True, False]:
 
+    degen_gm_energies = []
+    opset, mapping_line = green_mapping_line(n)
+    for gs_vector in degen_gs_vectors:
 
-# Galitskii_Migdal_energy
-n = len(h0)
-num_iterations = len(green)
-energy = []
-for k in range(0, num_iterations):
-    green_list = [g for g in green[k, :]]
-    mapping_line = green_mapping_full
+        true_stack_green = make_green_stack(gs_vector, opset, 31, n, down=down)
+        green = true_stack_green
+        num_iterations = len(green)
 
-    # prepare_position_maps
-    def convert_matpos(mat_pos):
-        r, c, k = mat_pos
-        assert r < n
-        assert c < n
-        assert k < len(green_list)
-        return r + c * n, k
+        energy = []
+        for k in range(0, num_iterations):
+            green_list = [g for g in green[k, :]]
 
-    R, C, D = (
-        np.zeros(len(mapping_line), dtype=np.int64),
-        np.zeros(len(mapping_line), dtype=np.int64),
-        np.zeros(len(mapping_line), dtype=np.complex128),
-    )
-    for i, (il, jl, kl, coeff) in enumerate(mapping_line):
-        R[i], C[i] = convert_matpos((il, jl, kl))
-        D[i] = coeff
-    position_maps = csr_array((D, (R, C)), shape=(n**2, len(green_list)))  # (16, 6)
+            # prepare_position_maps
+            def convert_matpos(mat_pos):
+                r, c, k = mat_pos
+                assert r < n
+                assert c < n
+                assert k < len(green_list)
+                return r + c * n, k
 
-    scalar_frequency_weights_function = lambda x: fermi(x)
-    matrix_frequency_constant = h0
-    GI = np.zeros(len(green_list))
-    for i, g in enumerate(green_list):
-        if not hasattr(g, "integrate"):
-            g = g.to_Lehmann()
-        GI[i] = g.integrate(scalar_frequency_weights_function)
-    fm = matrix_frequency_constant.flatten()
-    Kq = fm @ position_maps @ GI
+            R, C, D = (
+                np.zeros(len(mapping_line), dtype=np.int64),
+                np.zeros(len(mapping_line), dtype=np.int64),
+                np.zeros(len(mapping_line), dtype=np.complex128),
+            )
+            for i, (il, jl, kl, coeff) in enumerate(mapping_line):
+                R[i], C[i] = convert_matpos((il, jl, kl))
+                D[i] = coeff
+            position_maps = csr_array((D, (R, C)), shape=(n**2, len(green_list)))  # (16, 6)
 
-    scalar_frequency_weights_function = lambda x: x * fermi(x)
-    matrix_frequency_constant = np.eye(n)
-    GI = np.zeros(len(green_list))
-    for i, g in enumerate(green_list):
-        if not hasattr(g, "integrate"):
-            g = g.to_Lehmann()
-        GI[i] = g.integrate(scalar_frequency_weights_function)
-    fm = matrix_frequency_constant.flatten()
-    wG = fm @ position_maps @ GI
+            scalar_frequency_weights_function = lambda x: fermi(x)
+            matrix_frequency_constant = h_0
+            GI = np.zeros(len(green_list))
+            for i, g in enumerate(green_list):
+                if not hasattr(g, "integrate"):
+                    g = g.to_Lehmann()
+                GI[i] = g.integrate(scalar_frequency_weights_function)
+            fm = matrix_frequency_constant.flatten()
+            Kq = fm @ position_maps @ GI
 
-    energy.append((wG + Kq).real)  # /2 missing because spin degeneracy
+            scalar_frequency_weights_function = lambda x: x * fermi(x)
+            matrix_frequency_constant = np.eye(n)
+            GI = np.zeros(len(green_list))
+            for i, g in enumerate(green_list):
+                if not hasattr(g, "integrate"):
+                    g = g.to_Lehmann()
+                GI[i] = g.integrate(scalar_frequency_weights_function)
+            fm = matrix_frequency_constant.flatten()
+            wG = fm @ position_maps @ GI
 
-true_gm_energy = energy
+            energy.append((wG + Kq).real/2) #missing because spin degeneracy
 
-plt.plot(true_gm_energy)
+        degen_gm_energies.append(energy)
+    spin_gm_energies.append(degen_gm_energies)
+spin_gm_energies = np.asarray(spin_gm_energies)
 
-# %%
-E = true_gs_energy
 
 omega = np.linspace(0, 31, 200)
-
 real = omega
-imag = np.full_like(omega, E)
+imag = np.full_like(omega, true_gs_energy)
 
 plt.figure()
 
-plt.plot(real, imag, label="Energy line", color="red")
-plt.plot(true_gm_energy, color="blue")
-plt.grid()
+plt.plot(real, imag, label="true", color="black")
+for s, spin in enumerate(['up', 'down']):
+    for i, gs_vec in enumerate(degen_gs_vectors):
+        plt.plot(spin_gm_energies[s,i], label=f"degen gs {i} {spin}", lw=3-0.4*(i+s*2))
+
+plt.plot(spin_gm_energies.sum(0).mean(0), color="red", label=f"sum")
+plt.ylabel("energy")
+plt.xlabel("iteration")
+plt.title(f"{n} sites")
 
 plt.legend()
 plt.show()
 
-# %%
